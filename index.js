ef9d5acda87129ee66c7f03a7f611fc15d5c289c
Mikołaj Drygas <drygi@MacBook-Air-Mikoaj.local>
2018-11-27 21:34:19 +0100

    limit concurrent requests

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const userDBHelper = require('./dbHelpers/userDBHelper')(mySqlConnection)
 const oAuthModel = require('./authorization/accessTokenModel')(userDBHelper, accessTokenDBHelper)
 const oAuth2Server = require('node-oauth2-server')
 const express = require('express')
+const RateLimit = require('express-rate-limit');
 const expressApp = express()
+
+expressApp.enable('trust proxy')
+
+var apiLimiter = new RateLimit({
+  windowMs: 15*60*1000,
+  max: 100,
+  delayMs: 0
+})
 
 expressApp.oauth = oAuth2Server({
   model: oAuthModel,
@@ -20,7 +29,7 @@ const bodyParser = require('body-parser')
 
 expressApp.use(bodyParser.urlencoded({ extended: true }))
 expressApp.use(expressApp.oauth.errorHandler())
-expressApp.use('/auth', authRoutes)
+expressApp.use('/auth', authRoutes, apiLimiter)
 
 models.sequelize.sync().then(() => {
   expressApp.listen(port, () => {
